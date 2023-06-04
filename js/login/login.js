3f3b79e9ea3b845ef913ae46b8fc378a2dbca306
Sharon Babaran <sbabaran@techstyle.com>
2023-06-04 16:24:52 -0400

    Removed the unnecessary codes

--- js/login/login.js
+++ js/login/login.js
@@ -1,55 +1,46 @@
-// Get the total no of users
-if (getTotalUsers() === 0) {
-    deleteCookie('userInfo');
-    window.location = '../../templates/setup/setup.html';
-}
-else {
-    if (checkCookieExists('userInfo') === true) {
-        const cookieValue = getCookieValue('userInfo');
-        if (cookieValue) {
-            const { email, type } = cookieValue;
-            const userDetails = getUser(email);
-            if (email === userDetails.email && type === userDetails.type) {
-                window.location = getDashboardPage(userDetails.type);
-            }
-        }
-    }
-
-    // Handle form submission
-    function handleLoginFormSubmit(event) {
-        event.preventDefault();
-  
-        // Validate the form
-        if (!event.target.checkValidity()) {
-          event.stopPropagation();
-          event.target.classList.add('was-validated');
-          return;
-        }
-
-        // Reset form validation
-        event.target.classList.remove('was-validated');
-  
-        // Get form values
-        const loginEmail = document.getElementById('email').value;
-        const loginPassword = document.getElementById('password').value;
-
-        const userDetails = getUser(loginEmail);
-        if (userDetails.password === loginPassword) {
-            createCookie('userInfo', JSON.stringify({
-                email: loginEmail,
-                type: userDetails.type
-            }), 24);
+if (checkCookieExists('userInfo') === true) {
+    const cookieValue = getCookieValue('userInfo');
+    if (cookieValue) {
+        const { email, type } = cookieValue;
+        const userDetails = getUser(email);
+        if (email === userDetails.email && type === userDetails.type) {
             window.location = getDashboardPage(userDetails.type);
         }
-        else {
-            alert('Invalid user');
-        }
-  
     }
-  
-    // Add event listener to the login form
-    const loginForm = document.getElementById('loginForm');
-    loginForm.addEventListener('submit', handleLoginFormSubmit);
+}
 
+// Handle form submission
+function handleLoginFormSubmit(event) {
+    event.preventDefault();
+
+    // Validate the form
+    if (!event.target.checkValidity()) {
+        event.stopPropagation();
+        event.target.classList.add('was-validated');
+        return;
+    }
+
+    // Reset form validation
+    event.target.classList.remove('was-validated');
+
+    // Get form values
+    const loginEmail = document.getElementById('email').value;
+    const loginPassword = document.getElementById('password').value;
+
+    const userDetails = getUser(loginEmail);
+    if (userDetails.password === loginPassword) {
+        createCookie('userInfo', JSON.stringify({
+            email: loginEmail,
+            type: userDetails.type
+        }), 24);
+        window.location = getDashboardPage(userDetails.type);
+    }
+    else {
+        alert('Invalid user');
+    }
 
 }
+
+// Add event listener to the login form
+const loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit', handleLoginFormSubmit);
