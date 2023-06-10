e5d8ab113220fa97294c5aeec90146ca4aae1b33
Sharon Babaran <sbabaran@techstyle.com>
2023-06-10 02:06:57 -0400

    Fixed the css in the member edit page.

--- js/member/list.js
+++ js/member/list.js
@@ -34,15 +34,18 @@ function confirmDeleteUser(email) {
 
 // Display users
 function displayUsers(users) {
+    const chevron = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                    </svg>`;
     userTableBody.innerHTML = '';
 
     users.forEach(user => {
         const row = document.createElement('tr');
         row.innerHTML = `   
-            <td data-label="Email">${user.email}</td>
-            <td data-label="Hourly Rate">$ ${user.hourlyRate}</td>
-            <td data-label="Type">${(user.type == "admin") ? `<span class="badge badge-success">Admin</span>` : `<span class="badge badge-dark">Regular</span>`}</td>
-            <td data-label="Actions">
+            <td data-label="Email" tabindex="0" class="focusable">${user.email} <span class="chevron">${chevron}</span></td>
+            <td data-label="Hourly Rate" class="td-hidden">$ ${user.hourlyRate}</td>
+            <td data-label="Type" class="td-hidden">${(user.type == "admin") ? `<span class="badge badge-success">Admin</span>` : `<span class="badge badge-dark">Regular</span>`}</td>
+            <td data-label="Actions" class="td-hidden">
             <a class="btn btn-outline-dark btn-sm" href="edit_member.html?email=${user.email}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
