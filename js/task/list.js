9d2a3786176bbb34ba98e0ca62cc3e8cc29bb383
DILPREET BRAR <bdilpreet66@gmail.com>
2023-06-10 12:27:11 -0400

    added button padding

--- js/task/list.js
+++ js/task/list.js
@@ -31,20 +31,20 @@ const displayTasks = tasks => {
         tasks.forEach(task => {
             const row = document.createElement('tr');
             row.innerHTML = `   
-                <td data-label="Task ID">${task.id}</td>
-                <td data-label="Name">${task.name}</td>
-                <td data-label="Start Date">${task.startDate}</td>
-                <td data-label="End Date">${task.endDate}</td>
-                <td data-label="Assigned To">${task.assignedTo}</td>
-                <td data-label="Status">${getStatus(task.status)}</td>
-                <td data-label="Actions">
-                <a class="btn btn-outline-dark btn-sm" href="edit_task.html?id=${task.id}">
+                <td data-label="Task ID #" tabindex="0" class="focusable">${task.id}</td>
+                <td data-label="Name" class="td-hidden">${task.name}</td>
+                <td data-label="Start Date" class="td-hidden">${task.startDate}</td>
+                <td data-label="End Date" class="td-hidden">${task.endDate}</td>
+                <td data-label="Assigned To" class="td-hidden">${task.assignedTo}</td>
+                <td data-label="Status" class="td-hidden">${getStatus(task.status)}</td>
+                <td data-label="Actions" class="td-hidden">
+                <a class="btn btn-outline-dark btn-sm mb-1" href="edit_task.html?id=${task.id}">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                     </svg>
                 </a>
-                <button class="btn btn-outline-danger btn-sm" data-id="${task.id}" onclick="confirmDeleteTask('${task.id}')">
+                <button class="btn btn-outline-danger btn-sm mb-1" data-id="${task.id}" onclick="confirmDeleteTask('${task.id}')">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                     <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                 </svg>
