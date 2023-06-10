bad542fbe04ccf966e83a7275a5b6ae6b0a208e2
LCBO\stantoy <sbabaran@gmail.com>
2023-06-10 13:06:00 -0400

    Added the chevron down on the task list

--- js/task/list.js
+++ js/task/list.js
@@ -28,10 +28,13 @@ const displayTasks = tasks => {
     taskTableBody.innerHTML = '';
 
     if (tasks.length > 0){
+        const chevron = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                    </svg>`;
         tasks.forEach(task => {
             const row = document.createElement('tr');
             row.innerHTML = `   
-                <td data-label="Task ID #" tabindex="0" class="focusable">${task.id}</td>
+                <td data-label="Task ID #" tabindex="0" class="focusable">${task.id} <span class="chevron">${chevron}</span></td>
                 <td data-label="Name" tabindex="0" class="focusable">${task.name}</td>
                 <td data-label="Start Date" class="td-hidden">${task.startDate}</td>
                 <td data-label="End Date" class="td-hidden">${task.endDate}</td>
